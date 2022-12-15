275d70c6ffd86dd04a7323d3b9f31113b36488a3
Pavel Arabei <Ptashkaaaaaa@gmail.com>
2022-12-15 16:18:22 +0300

    feat: webpack and es config

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const EslingPlugin = require('eslint-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpacPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    context: path.join(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        index: './index',
+    },
+    output: {
+        filename: '[name].[contenthash].js',
+        //путь к итоговому файлу
+        path: path.join(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: ['.ts', '.json', '.js'],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    plugins: [
+        new EslingPlugin({ extensions: 'ts' }),
+        new HTMLWebpackPlugin({
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+
+        new CopyWebpacPlugin({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'src', 'favicon.ico'),
+                    to: path.join(__dirname, 'dist'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    //devServer: {
+    //    port: 4200,
+    //},
+    module: {
+        rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader'],
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader'],
+            },
+            {
+                test: /\.csv$/,
+                use: ['csv-loader'],
+            },
+        ],
+    },
+};
+
+
+//в файле pacege.json
+//"scripts": {
+//  "dev": "webpack --mode development",
+//  "build": "webpack --mode production"
+//  "watch": "webpack --mode development --watch"
+//} -  и далее для запуска вебпака используем команыды
+//npm run dev - запуск в режиме разработки
+//npm run build - запуск в режиме минификации
+//"watch" - будет постоянно следить за изменением файлов
