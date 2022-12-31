634b3543b30a7abdad9009259c9c01524e3de057
Pavel Arabei <Ptashkaaaaaa@gmail.com>
2022-12-31 16:59:33 +0300

    feat: add modal-window

--- src/app/appView.ts
+++ src/app/appView.ts
@@ -1,6 +1,7 @@
 import { HeaderView } from './components/header/view';
 import { SearchPage } from './pages/searchPage/view';
 import { SearchPageController } from './pages/searchPage/controller';
+//!import { ModalWindow } from './components/modal-window/modal-window';
 
 export class AppView {
   header: HeaderView;
@@ -8,15 +9,16 @@ export class AppView {
   documentBody: HTMLElement;
   container: HTMLElement;
   searchPageController: SearchPageController;
+  //!modalWindow: ModalWindow;
   constructor() {
     this.searchPageController = new SearchPageController();
-    //this.searchPage = new SearchPage();
     this.searchPage = this.searchPageController.app;
     this.container = document.createElement('div');
     this.container.classList.add('container');
     this.documentBody = document.body;
     this.header = new HeaderView();
     this.documentBody.append(this.header.container, this.container);
+    //!this.modalWindow = new ModalWindow();
   }
 
   set containerContent(x: HTMLElement) {
